be202f0ad1237c1b05e4bdc58334e5a6310b01b1
iurii <1arxemond1@gmail.com>
2023-09-10 17:00:45 -0500

    selemium and jasmine tests were added

---
+++ README.md
@@ -0,0 +1,34 @@
+# Run tests
+
+1) Install node.js .\tests\selenium.js
+
+2) Install seleinum. This is a tool for interactive tasting
+
+```bash
+
+npm i selenium-webdriver
+npm install mocha chai --save-dev
+
+```
+
+3) Launch the test scenario
+
+```bash
+
+node .\tests\selenium.js 
+
+```
+
+4) An interactive windows will pop up and you'll see the whole test scenario
+
+# Unit tests
+npm init -y
+## Install Jasmine and Jasmine Node.js Runner
+npm install --save-dev jasmine jasmine-node
+npm install jquery
+npm install jsdom
+##
+```bash
+npx jasmine .\unit-tests.js
+```
+
--- tests/selenium.js
+++ tests/selenium.js
@@ -1,122 +1,97 @@
-// const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-//
-// (async function example() {
-//     let driver = await new Builder().forBrowser(Browser.CHROME).build();
-//     try {
-//         await driver.get('http://localhost:63342/reviser/GREdictionary.html');
-//         // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//         await driver.wait(until.titleIs('webdriver - Google Search'), 10000);
-//     } finally {
-//         await driver.quit();
-//     }
-// })();
-/*const { Builder, By, Key, until } = require('selenium-webdriver');
+const {Builder, Key, until, By} = require('selenium-webdriver');
+const chai = require('chai');
+const { assert } = chai;
+
+chai.should();
 
 async function runTest() {
     const driver = await new Builder().forBrowser('chrome').build();
 
     try {
-        await driver.get('http://localhost:63342/reviser/GREdictionary.html');
-        // await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
-        await driver.wait(until.titleIs('Selenium - Google Search'), 500000);
-    } finally {
-        await driver.quit();
-    }
-}
+        await driver.get('http://arxemond.ru/reviser-demo/GREdictionary.html?token=true');
 
-runTest();*/
+        var element = await driver.findElement(By.id('word'));
+        var value = await element.getText();
+        console.log(">>>" + value);
+        assert.equal(value,'');
 
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+        // Get today's date in the format "mm/dd/yyyy"
+        const today = new Date();
+        const formattedDate = `${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getDate().toString().padStart(2, '0')}/${today.getFullYear()}`;
+        const script = `
+            const data = localStorage.getItem('${formattedDate}');
+            return data;
+        `;
+        var revisedWordsForToday = await driver.executeScript(script);
+        assert.equal(revisedWordsForToday, null);
 
-// // Get the directory where this script is located
-// const scriptDirectory = __dirname;
-//
-// // Create a simple HTTP server to serve the HTML file
-// const server = http.createServer((req, res) => {
-//     console.log(req.url)
-//
-//     // Normalize the file path to avoid directory traversal vulnerabilities
-//     filePath = path.normalize(req.url);
-//
-//     // Construct the absolute path to the requested file
-//     const absoluteFilePath = path.join(scriptDirectory, filePath);
-//     console.log(absoluteFilePath);
-//
-//     // Get the content type based on the file extension
-//     const contentType = getContentType(absoluteFilePath);
-//
-//     // Read and serve the file
-//     fs.readFile(absoluteFilePath, 'utf8', (err, data) => {
-//         if (err) {
-//             // console.log(err);
-//             res.writeHead(404);
-//             res.end('File not found');
-//         } else {
-//
-//             res.writeHead(200, { 'Content-Type': contentType });
-//             res.end(data);
-//         }
-//     });
-//     // if (req.url === '/GREdictionary.html') {
-//     //     fs.readFile('GREdictionary.html', 'utf8', (err, data) => {
-//     //         if (err) {
-//     //             res.writeHead(404);
-//     //             res.end('File not found');
-//     //         } else {
-//     //             res.writeHead(200, { 'Content-Type': 'text/html' });
-//     //             res.end(data);
-//     //         }
-//     //     });
-//     // } else {
-//     //     res.writeHead(404);
-//     //     res.end('Page not found');
-//     // }
-// });
+        const cntName = 'TextToAudioService.cnt';
+        cntValue = await driver.executeScript(`return ${cntName};`);
+        assert.equal(cntValue, 0);
 
-// server.listen(8080, 'localhost', () => {
-//     console.log('Server is running at http://localhost:8080');
-// });
+        await sleep(1000);
 
-// Selenium WebDriver test
-async function runTest() {
-    const driver = await new Builder().forBrowser('chrome').build();
+        await driver.actions().sendKeys(Key.RIGHT).perform();
+        element = await driver.findElement(By.id('word'));
+        value = await element.getText();
+        console.log(">>>" + value);
+        assert.notEqual(value, '');
 
-    try {
-        await driver.get('http://arxemond.ru/reviser-demo/GREdictionary.html');
-        // Perform Selenium actions on the loaded HTML file
-        // For example, you can find elements and interact with them here
-        // For demonstration, let's get the title of the page
-        // await driver.wait(until.elementLocated(By.css('link[href="css/css.css"]')), 10000);
+        revisedWordsForToday = await driver.executeScript(script);
+        assert.equal(revisedWordsForToday, 1);
+
+        await driver.actions().sendKeys(Key.LEFT).perform();
+        assert.notEqual(value, '');
+
+        cntValue = await driver.executeScript(`return ${cntName};`);
+        assert.equal(cntValue, 0);
+
+        const buttonElement = await driver.findElement(By.id('audioTranslate'));
+        // imitate 3 clicks very fast. only 1 pronunciation should happen
+        await sleep(100);
+        for (let i = 0; i < 3; i++) {
+            await buttonElement.click();
+            await sleep(200);
+        }
+
+        await sleep(3000);
+        cntValue = await driver.executeScript(`return ${cntName};`);
+        console.log(">>>" + cntValue);
+        assert.equal(cntValue, 1);
+
+        let maxVal = 1000;
+        while (maxVal-- > 0 && await driver.findElement(By.id('token')).getText() === "") {
+            if (maxVal === 0) throw new Error("could not find synonyms");
+
+            await driver.actions().sendKeys(Key.RIGHT).perform();
+        }
 
         const title = await driver.getTitle();
         console.log('Page Title:', title);
         await driver.wait(until.titleIs('Selenium - Google Search'), 500000);
     } finally {
         await driver.quit();
-        // Close the HTTP server when done
-        server.close(() => {
-            console.log('Server closed.');
-        });
     }
 }
 
-// Function to determine the content type based on file extension
-function getContentType(filePath) {
-    const extname = path.extname(filePath);
-    switch (extname) {
-        case '.html':
-            return 'text/html';
-        case '.js':
-            return 'application/javascript';
-        case '.css':
-            return 'text/css';
-        // Add more content types as needed
-        default:
-            return 'text/plain';
-    }
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// // Function to determine the content type based on file extension
+// function getContentType(filePath) {
+//     const extname = path.extname(filePath);
+//     switch (extname) {
+//         case '.html':
+//             return 'text/html';
+//         case '.js':
+//             return 'application/javascript';
+//         case '.css':
+//             return 'text/css';
+//         // Add more content types as needed
+//         default:
+//             return 'text/plain';
+//     }
+// }
 
 runTest();
